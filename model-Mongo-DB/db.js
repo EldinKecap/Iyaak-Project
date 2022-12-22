f1a92334d7f87bf4696ec744354577d9d61c90fc
Eldin Kecap <eldinkecap@gmail.com>
2022-12-22 19:05:38 +0100

    Removed mongoDB model

--- model-Mongo-DB/db.js
+++
@@ -1,8 +0,0 @@
-const { MongoClient } = require("mongodb");
-
-const uri = "mongodb+srv://root:root@cluster0.ze2oyhf.mongodb.net/?retryWrites=true&w=majority";
-
-let client = new MongoClient(uri);
-
-module.exports = {client}
-
