21b9e73a48b208287b36684d1f9b4a5412708beb
Eldin Kecap <eldinkecap@gmail.com>
2023-01-07 21:38:29 +0100

    added front end

--- controller/userController.js
+++ controller/userController.js
@@ -27,6 +27,8 @@ userController.getUser = async( req, res ) => {
 }
 
 userController.create = async( req, res ) => {
+    console.log(req.body);
+
     try {
         let result = await User.create( req.body );
         res.status(201).json( result );
--- server.js
+++ server.js
@@ -2,12 +2,17 @@ const express = require('express');
 const mongoose = require('mongoose');
 const userRouter = require('./router/userRouter')
 const app = express();
-const port = 3000;
+const port = 5000;
 const uri = "mongodb+srv://root:root@cluster0.ze2oyhf.mongodb.net/?retryWrites=true&w=majority";
+const cors = require('cors')
+
 
 mongoose.connect(uri, { dbName: 'IiyakProject' });
 
 app.use( express.json() );
+
+app.use(cors());
+
 app.use( '/user', userRouter );
 
 app.all( '*', ( req, res ) => {
